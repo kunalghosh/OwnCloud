eaa2f1b58baa7b548cd8b2e40510d1635c00b876
Kunal Ghosh <kunal.t2@gmail.com>
2011-07-18 03:36:36 -0400

    Changed logging function to see if it logs embedded arrays or not

--- fileSync/logging.php
+++ fileSync/logging.php
@@ -4,7 +4,8 @@ function lg($str) {
 	global $do_log, $base_dir;
 	if ($do_log > 0) {
 		$hand_lg = fopen("$base_dir/log.txt", "a");
-		fwrite($hand_lg, $str."\r\n");
+		$str_new = implode(" ",$str); #just to see if this logs arrays embedded in strings or not.
+		fwrite($hand_lg, $str_new."\r\n");
 		fclose($hand_lg);
 	};
 };

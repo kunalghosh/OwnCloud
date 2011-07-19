3a2942414954d6bebeb98b4edc71d657fb044acb
Kunal Ghosh <kunal.t2@gmail.com>
2011-07-18 20:41:45 -0400

    Reverted to old logging method, the printing arrays (associative) part doesn't seem to work right now. Will look into it later

--- fileSync/logging.php
+++ fileSync/logging.php
@@ -4,8 +4,7 @@ function lg($str) {
 	global $do_log, $base_dir;
 	if ($do_log > 0) {
 		$hand_lg = fopen("$base_dir/log.txt", "a");
-		$str_new = implode(" ",$str); #just to see if this logs arrays embedded in strings or not.
-		fwrite($hand_lg, $str_new."\r\n");
+		fwrite($hand_lg, $str."\r\n");
 		fclose($hand_lg);
 	};
 };

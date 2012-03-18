a250c8c51cedd5bd3a1735e405b808e04f463a65
Kunal Ghosh <kunal.t2@gmail.com>
2012-03-18 22:07:24 +0530

    Fixed the database schema , this link http://www.sulc.edu/sulcalumni/app/lib/pear/docs/MDB2_Schema/docs/xml_schema_documentation.html came very handy there's no varchar in MDB xml its just called text :P

--- apps/fileSync/appinfo/app.php
+++ apps/fileSync/appinfo/app.php
@@ -9,7 +9,7 @@ OC_APP::addNavigationEntry(array(
     "id" => "fileSync_editor",
     "order" => 10,
     "href" => OC_HELPER::linkTo("fileSync", "editor.php"),
-    "icon" => OC_HELPER::imagePath("fileSync", "icon.png"),
+    "icon" => OC_HELPER::imagePath("fileSync", "output4.png"),
     "name" => "fileSync"));
 
 #OC_APP::addSettingsPage(array(
